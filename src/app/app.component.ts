f6ff30ed57c12518088292c613d1ea17aa130102
David00691 <wyrwadawid1@gmai.com>
2023-10-07 19:39:58 +0200

    Finished, today, tomorrow, etc. toolbar, geolocation TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GeolocationService } from './geolocation.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'forecast-app';
+  locationMessage = '';
+
+  constructor(private geolocationService: GeolocationService) {}
+
+  ngOnInit() {
+    this.geolocationService.getLocation()
+      .then(position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        this.locationMessage = `Latitude: ${latitude}, Longitude: ${longitude}`;
+      })
+      .catch(error => {
+        console.error("Error getting location: " + error.message);
+        this.locationMessage = "Error getting location";
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { ForecasttableComponent } from './forecasttable/forecasttable.component';
+import {MatListModule} from "@angular/material/list";
+import {MatLegacyMenuModule} from "@angular/material/legacy-menu";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    ForecasttableComponent,
+    MatListModule,
+    MatLegacyMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
